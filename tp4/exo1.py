6d4576bad8db8c8e848ca47735786a72a52ed5bb
hRebb <hamza.rebb@gmail.com>
2023-10-23 15:39:45 +0100

    add a new exercise

---
+++ tp4/exo1.py
@@ -0,0 +1,13 @@
+# Citi Bike est le système de vélos en libre-service de la ville de New York. Une API est disponible, permettant d’obtenir des informations en temps réel : 
+# https://gbfs.citibikenyc.com/gbfs/fr/station_information.json
+# https://gbfs.citibikenyc.com/gbfs/fr/station_status.json
+# Installez la lib Requests, qui vous permettra de faire des requêtes web afin d'interroger cette API.
+
+import json
+import requests
+
+def get_station_information():
+    response = requests.get("https://gbfs.citibikenyc.com/gbfs/fr/station_information.json")
+    return json.dumps(json.loads(response.content), indent=4)
+
+print(get_station_information())
